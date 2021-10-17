74c2bce041a850d4b91823eff6fc75e1df1a8744
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-10-17 12:28:46 +0700

    Re-style Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/Button/Button";
+import HeroSlide from "../components/HeroSlide/HeroSlide";
+import MovieList from "../components/MovieList/MovieList";
+import { category, movieType, tvType } from "../api/tmdbApi";
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
-    )
+        <>
+            <HeroSlide />
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Trending Movies</h2>
+                        <Link to="/movie">
+                            <OutlineButton className="small">
+                                View more
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList
+                        category={category.movie}
+                        type={movieType.popular}
+                    />
+                </div>
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Top Rated Movies</h2>
+                        <Link to="/movie">
+                            <OutlineButton className="small">
+                                View more
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList
+                        category={category.movie}
+                        type={movieType.top_rated}
+                    />
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Trending TV</h2>
+                        <Link to="/tv">
+                            <OutlineButton className="small">
+                                View more
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.popular} />
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Top Rated TV</h2>
+                        <Link to="/tv">
+                            <OutlineButton className="small">
+                                View more
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.top_rated} />
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Home
+export default Home;
